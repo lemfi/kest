de855bc3cd56f7ce3572c6fa91b7291f07df2dc0
lemfi <lemfi.22@gmail.com>
2020-12-14 23:36:08 +0100

    prepare version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group  = "com.github.lemfi.kest"
-version = "0.0.5"
+version = "0.0.6"
 
 val kotlinVersion : String by extra { "1.4.10" }
 val coroutineVersion : String by extra { "1.4.1" }
