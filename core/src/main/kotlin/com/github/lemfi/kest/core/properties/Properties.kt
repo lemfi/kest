7ebcf50cccd414a9414539496f1f4d199cae9a2d
lemfi <lemfi.22@gmail.com>
2021-05-31 23:23:42 +0200

    load configuration from either system properties or environment variables, defaults to "/kest.yml"

--- core/src/main/kotlin/com/github/lemfi/kest/core/properties/Properties.kt
+++ core/src/main/kotlin/com/github/lemfi/kest/core/properties/Properties.kt
@@ -1,6 +1,9 @@
 package com.github.lemfi.kest.core.properties
 
+import com.sksamuel.hoplite.ConfigFailure
 import com.sksamuel.hoplite.ConfigLoader
+import com.sksamuel.hoplite.ConfigResult
+import com.sksamuel.hoplite.fp.invalid
 import org.slf4j.LoggerFactory
 
 val config: MutableMap<Class<*>, Any> = mutableMapOf()
@@ -8,8 +11,16 @@ val config: MutableMap<Class<*>, Any> = mutableMapOf()
 inline fun <reified E : Any, R> property(l: E.() -> R): R {
     val conf =
         when (config.get(E::class.java)) {
-            null -> ConfigLoader().loadConfigOrThrow<E>("/${System.getProperty("kest-conf", "kest.yml")}")
-                .apply { config[E::class.java] = this }
+            null ->
+                listOf(System.getProperty("kest-conf", ""), System.getenv("kest-conf"), "/kest.yml").let { source ->
+                    var configuration: ConfigResult<E> = ConfigFailure.UnknownSource("").invalid()
+                    val sourcesIterator = source.iterator()
+                    while (configuration.isInvalid() && sourcesIterator.hasNext()) {
+                        configuration =  ConfigLoader().loadConfig(sourcesIterator.next())
+                    }
+                    configuration.getUnsafe()
+                }
+                    .apply { config[E::class.java] = this }
             else -> config.get(E::class.java) as E
         }
     return conf.l()
