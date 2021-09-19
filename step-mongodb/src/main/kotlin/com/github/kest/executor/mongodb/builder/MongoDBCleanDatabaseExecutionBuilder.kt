ca7463c6e5b0e9ebacdc4d58f8a56778c76636ea
lemfi <lemfi.22@gmail.com>
2021-09-19 17:56:44 +0200

    remove collection from db clean execution builder

--- step-mongodb/src/main/kotlin/com/github/kest/executor/mongodb/builder/MongoDBCleanDatabaseExecutionBuilder.kt
+++ step-mongodb/src/main/kotlin/com/github/kest/executor/mongodb/builder/MongoDBCleanDatabaseExecutionBuilder.kt
@@ -7,15 +7,12 @@ import com.github.lemfi.kest.core.model.Execution
 
 class MongoDBCleanDatabaseExecutionBuilder : ExecutionBuilder<Unit> {
 
-    lateinit var collection: String
-
     var connection = mongoDBProperty { connection }
     var database = mongoDBProperty { database }
 
 
     override fun toExecution(): Execution<Unit> {
         return MongoDBCleanDatabaseExecution(
-            collection,
             connection,
             database,
         )
