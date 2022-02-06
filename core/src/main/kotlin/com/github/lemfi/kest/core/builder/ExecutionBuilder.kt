62a71111450ed85990ae476ab48dea87f80e19c9
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-02-06 14:46:52 +0100

    introduce a function to specify dependencies between steps => useful for steps that return nothing

--- core/src/main/kotlin/com/github/lemfi/kest/core/builder/ExecutionBuilder.kt
+++ core/src/main/kotlin/com/github/lemfi/kest/core/builder/ExecutionBuilder.kt
@@ -1,8 +1,18 @@
 package com.github.lemfi.kest.core.builder
 
 import com.github.lemfi.kest.core.model.Execution
+import com.github.lemfi.kest.core.model.IStepPostExecution
 
 interface ExecutionBuilder<T> {
 
     fun toExecution(): Execution<T>
+
+    infix fun depends.on(stepResult: IStepPostExecution<*, *>) {
+        stepResult()
+    }
+
 }
+
+@Suppress("ClassName")
+object depends
+
