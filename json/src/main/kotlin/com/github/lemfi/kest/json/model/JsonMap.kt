af4712b1f2c9d2272e149933ffcb8f27601fda19
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-02-24 22:42:19 +0100

    improve JsonMap getForPath

--- json/src/main/kotlin/com/github/lemfi/kest/json/model/JsonMap.kt
+++ json/src/main/kotlin/com/github/lemfi/kest/json/model/JsonMap.kt
@@ -1,5 +1,7 @@
 package com.github.lemfi.kest.json.model
 
+import org.opentest4j.AssertionFailedError
+
 class JsonMap : HashMap<String, Any>() {
 
     /**
@@ -8,19 +10,39 @@ class JsonMap : HashMap<String, Any>() {
      * For picking nth element of an array use &#91;n&#93; notation
      */
     @Suppress("unchecked_cast")
-    fun getForPath(vararg keys: String): Any? {
+    inline fun <reified T> getForPath(vararg keys: String): T? {
         return keys.fold(this as Any?) { res, key ->
             if (key.contains("[")) {
-                key.split("[").map { it.replace("]", "") }.let { it.first() to it.subList(1, it.size) }.let {
-                    it.second.fold((res as Map<String, Any?>)[it.first]) { elem, key ->
-                        (elem as List<Any?>)[key.toInt()]
+                key
+                    .split("[")
+                    .map { it.replace("]", "") }
+                    .let { it.first() to it.subList(1, it.size) }
+                    .let { (index, path) ->
+                        path.fold((res as Map<String, Any?>)[index]) { elem, key ->
+                            (elem as List<Any?>)[key.toInt()]
+                        }
                     }
-                }
             } else {
-                (res as Map<String, Any?>)[key]
+                ((res as Map<String, Any?>)[key])
             }
+        }?.let {
+            if (it is T) it else throw AssertionFailedError("expected ${T::class}, was ${it::class} ($it)")
         }
     }
+
+    /**
+     * get a value from its path in json
+     *
+     * For picking nth element of an array use &#91;n&#93; notation
+     */
+    @Suppress("unchecked_cast")
+    @JvmName("getAnyForPath")
+    @Deprecated(
+        message = "Consider using typed function",
+        replaceWith = ReplaceWith("this.getForPath<Any?>(*keys)"),
+        level = DeprecationLevel.ERROR
+    )
+    fun getForPath(vararg keys: String): Any? = getForPath<Any?>(*keys)
 }
 
 class JsonArray : KestArray<JsonMap>()
--- json/src/test/kotlin/com/github/lemfi/kest/json/model/JsonMapTest.kt
+++ json/src/test/kotlin/com/github/lemfi/kest/json/model/JsonMapTest.kt
@@ -2,6 +2,8 @@ package com.github.lemfi.kest.json.model
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.opentest4j.AssertionFailedError
 
 class JsonMapTest {
 
@@ -29,28 +31,67 @@ class JsonMapTest {
 
         }
 
-        Assertions.assertEquals("value", json.getForPath("key"))
-        Assertions.assertEquals("value2", json.getForPath("key2"))
-        Assertions.assertEquals("value31", json.getForPath("key3", "key31"))
-        Assertions.assertEquals("value32", json.getForPath("key3", "key32"))
-        Assertions.assertEquals(true, json.getForPath("key3", "key33[0]"))
-        Assertions.assertEquals("a", json.getForPath("key3", "key34[0][0]"))
-        Assertions.assertEquals("b", json.getForPath("key3", "key34[0][1]"))
-        Assertions.assertEquals("c", json.getForPath("key3", "key34[1][0]"))
-        Assertions.assertEquals("d", json.getForPath("key3", "key34[1][1]"))
+        Assertions.assertEquals("value", json.getForPath<Any?>("key"))
+        Assertions.assertEquals("value2", json.getForPath<Any?>("key2"))
+        Assertions.assertEquals("value31", json.getForPath<Any?>("key3", "key31"))
+        Assertions.assertEquals("value32", json.getForPath<Any?>("key3", "key32"))
+        Assertions.assertEquals(true, json.getForPath<Any?>("key3", "key33[0]"))
+        Assertions.assertEquals("a", json.getForPath<Any?>("key3", "key34[0][0]"))
+        Assertions.assertEquals("b", json.getForPath<Any?>("key3", "key34[0][1]"))
+        Assertions.assertEquals("c", json.getForPath<Any?>("key3", "key34[1][0]"))
+        Assertions.assertEquals("d", json.getForPath<Any?>("key3", "key34[1][1]"))
 
-        Assertions.assertEquals("val11", json.getForPath("array[0]", "elem11"))
-        Assertions.assertEquals("val12", json.getForPath("array[0]", "elem12"))
-        Assertions.assertEquals(1, json.getForPath("array[0]", "elem13[0]"))
-        Assertions.assertEquals(2, json.getForPath("array[0]", "elem13[1]"))
-        Assertions.assertEquals(3, json.getForPath("array[0]", "elem13[2]"))
-        Assertions.assertEquals(4, json.getForPath("array[0]", "elem13[3]"))
+        Assertions.assertEquals("val11", json.getForPath<Any?>("array[0]", "elem11"))
+        Assertions.assertEquals("val12", json.getForPath<Any?>("array[0]", "elem12"))
+        Assertions.assertEquals(1, json.getForPath<Any?>("array[0]", "elem13[0]"))
+        Assertions.assertEquals(2, json.getForPath<Any?>("array[0]", "elem13[1]"))
+        Assertions.assertEquals(3, json.getForPath<Any?>("array[0]", "elem13[2]"))
+        Assertions.assertEquals(4, json.getForPath<Any?>("array[0]", "elem13[3]"))
 
-        Assertions.assertEquals("val21", json.getForPath("array[1]", "elem21"))
-        Assertions.assertEquals("val22", json.getForPath("array[1]", "elem22"))
-        Assertions.assertEquals(5, json.getForPath("array[1]", "elem23[0]"))
-        Assertions.assertEquals(6, json.getForPath("array[1]", "elem23[1]"))
-        Assertions.assertEquals(7, json.getForPath("array[1]", "elem23[2]"))
-        Assertions.assertEquals(8, json.getForPath("array[1]", "elem23[3]"))
+        Assertions.assertEquals("val21", json.getForPath<Any?>("array[1]", "elem21"))
+        Assertions.assertEquals("val22", json.getForPath<Any?>("array[1]", "elem22"))
+        Assertions.assertEquals(5, json.getForPath<Any?>("array[1]", "elem23[0]"))
+        Assertions.assertEquals(6, json.getForPath<Any?>("array[1]", "elem23[1]"))
+        Assertions.assertEquals(7, json.getForPath<Any?>("array[1]", "elem23[2]"))
+        Assertions.assertEquals(8, json.getForPath<Any?>("array[1]", "elem23[3]"))
+    }
+
+    @Test
+    fun `navigation in json is operational and can be typed`() {
+
+        val json = JsonMap().apply {
+            put("key", "value")
+            put("key2", "value2")
+            put("key3", JsonMap().apply {
+                put("key31", "value31")
+                put("key32", "value32")
+                put("key33", listOf(true, false))
+                put("key34", listOf(listOf("a", "b"), listOf("c", "d")))
+            })
+            put("array", listOf(JsonMap().apply {
+                put("elem11", "val11")
+                put("elem12", "val12")
+                put("elem13", listOf(1, 2, 3, 4))
+            }, JsonMap().apply {
+                put("elem21", "val21")
+                put("elem22", "val22")
+                put("elem23", listOf(5, 6, 7, 8))
+            }))
+
+        }
+
+        Assertions.assertEquals(JsonMap().apply {
+            put("key31", "value31")
+            put("key32", "value32")
+            put("key33", listOf(true, false))
+            put("key34", listOf(listOf("a", "b"), listOf("c", "d")))
+        }, json.getForPath<JsonMap>("key3"))
+
+        val exception = assertThrows<AssertionFailedError> {
+            json.getForPath<Number>("key")
+        }
+
+        Assertions.assertEquals("expected class kotlin.Number, was class kotlin.String (value)", exception.message)
+
     }
 }
