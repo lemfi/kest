0b0f0ccec2e72bfec08cbdb5d62b9d78e8d88450
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-04-05 23:08:39 +0200

    Make stacktrace filtering configurable

--- core/src/main/kotlin/com/github/lemfi/kest/core/model/FilteredAssertionFailedError.kt
+++ core/src/main/kotlin/com/github/lemfi/kest/core/model/FilteredAssertionFailedError.kt
@@ -1,17 +1,21 @@
 package com.github.lemfi.kest.core.model
 
+import com.github.lemfi.kest.core.properties.assertionProperty
 import org.opentest4j.AssertionFailedError
 
-class FilteredAssertionFailedError(message: String?, expected: Any?, actual: Any?, throwable: Throwable? = null): AssertionFailedError(message, expected, actual, throwable) {
-    constructor(message: String?, throwable: Throwable): this(message, null, null, throwable)
-    constructor(message: String?): this(message, null, null, null)
+class FilteredAssertionFailedError(message: String?, expected: Any?, actual: Any?, throwable: Throwable? = null) :
+    AssertionFailedError(message, expected, actual, throwable) {
+    constructor(message: String?, throwable: Throwable) : this(message, null, null, throwable)
+    constructor(message: String?) : this(message, null, null, null)
 
     init {
-        val packageNameToFilter = stackTrace.getOrNull(0)?.className?.substringBeforeLast(".")
-        stackTrace = packageNameToFilter?.let {
-            stackTrace.mapNotNull {
-                if (it.className.contains(packageNameToFilter)) null else it
-            }.toTypedArray()
-        } ?: stackTrace
+        if (assertionProperty { filterStackTraces }) {
+            val packageNameToFilter = stackTrace.getOrNull(0)?.className?.substringBeforeLast(".")
+            stackTrace = packageNameToFilter?.let {
+                stackTrace.mapNotNull {
+                    if (it.className.contains(packageNameToFilter)) null else it
+                }.toTypedArray()
+            } ?: stackTrace
+        }
     }
 }
---
+++ core/src/main/kotlin/com/github/lemfi/kest/core/properties/AssertionProperties.kt
@@ -0,0 +1,21 @@
+package com.github.lemfi.kest.core.properties
+
+import org.slf4j.LoggerFactory
+
+internal data class AssertionProperties(
+    val assertions: AssertionProp
+)
+
+data class AssertionProp(
+    val filterStackTraces: Boolean = true,
+)
+
+internal fun <R> assertionProperty(l: AssertionProp.() -> R): R {
+    val shortcut: AssertionProperties.() -> R = { assertions.l() }
+    return try {
+        property(shortcut)
+    } catch (e: Throwable) {
+        LoggerFactory.getLogger("Core-Kest").debug("No configuration found for assertions, use default values")
+        AssertionProp().l()
+    }
+}
