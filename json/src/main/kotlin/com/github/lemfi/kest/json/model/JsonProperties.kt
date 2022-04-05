995b70659d335646dbd1f8cf90c4acdcb662ac7b
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-04-05 22:54:56 +0200

    typo

--- json/src/main/kotlin/com/github/lemfi/kest/json/model/JsonProperties.kt
+++ json/src/main/kotlin/com/github/lemfi/kest/json/model/JsonProperties.kt
@@ -3,14 +3,17 @@ package com.github.lemfi.kest.json.model
 import com.github.lemfi.kest.core.properties.property
 import org.slf4j.LoggerFactory
 
-data class JsonProperties(val checkArraysOrder: Boolean = true)
+data class JsonProperties(
+    val checkArraysOrder: Boolean = true,
+    val ignoreUnknownProperties: Boolean = false,
+)
 
 internal fun <R> jsonProperty(l: JsonProperties.() -> R): R {
     val shortcut: JsonProperties.() -> R = { l() }
     return try {
         property(shortcut)
     } catch (e: Throwable) {
-        LoggerFactory.getLogger("RABBITMQ-Kest").debug("No configuration found for rabbitmq, use default values")
+        LoggerFactory.getLogger("JSON-Kest").debug("No configuration found for json, use default values")
         JsonProperties().l()
     }
 }
