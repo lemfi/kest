a2879e0e219fdbaa8c59b5173dc664e66bf03cbb
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-06-09 22:59:59 +0200

    prepare version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.lemfi.kest"
-version = "0.6.7"
+version = "0.6.8"
 
 buildscript {
     val kotlinVersion: String by project
