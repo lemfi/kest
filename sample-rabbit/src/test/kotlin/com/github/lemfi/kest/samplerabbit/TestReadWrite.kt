96fb28bca4a2bfba913b9df0de0ae11c444dab4d
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-07-20 10:17:35 +0200

    use management api to count messages in queue

--- sample-rabbit/src/test/kotlin/com/github/lemfi/kest/samplerabbit/TestReadWrite.kt
+++ sample-rabbit/src/test/kotlin/com/github/lemfi/kest/samplerabbit/TestReadWrite.kt
@@ -9,6 +9,7 @@ import com.github.lemfi.kest.junit5.runner.`play scenario`
 import com.github.lemfi.kest.rabbitmq.cli.`create rabbitmq queue`
 import com.github.lemfi.kest.rabbitmq.cli.`given message from rabbitmq queue`
 import com.github.lemfi.kest.rabbitmq.cli.`given messages from rabbitmq queue`
+import com.github.lemfi.kest.rabbitmq.cli.`given number of messages in rabbitmq queue`
 import com.github.lemfi.kest.rabbitmq.cli.`publish rabbitmq message`
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -45,29 +46,41 @@ class TestReadWrite {
             eq("Au secours obi-wan_kenobi, vous êtes notre seul espoir !", it.last().message)
         }
 
+        `given number of messages in rabbitmq queue` {
+            queue = "obi-wan_kenobi"
+        } `assert that` {
+            eq(0L, it.total)
+        }
     }
 
     @TestFactory
-    fun `Leia asks for help - check broadcast once`() = `play scenario`(name = "Leia asks for help via RabbitMQ, check broadcast once") {
+    fun `Leia asks for help - check broadcast once`() =
+        `play scenario`(name = "Leia asks for help via RabbitMQ, check broadcast once") {
 
-        `create rabbitmq queue`("Obi-Wan Kenobi should be born before he can receive a message") {
-            `create queue` { "obi-wan_kenobi" }
+            `create rabbitmq queue`("Obi-Wan Kenobi should be born before he can receive a message") {
+                `create queue` { "obi-wan_kenobi" }
+            }
+
+            `publish rabbitmq message`("declare that R2D2 might deliver a message to Obi-Wan Kenobi") {
+                publish { "obi-wan_kenobi" } `to exchange` "" `with routing key` "R2D2"
+            }
+
+            `given message from rabbitmq queue`<String>(
+                name = "message from Leia was broadcasted",
+                retry = 10.times `by intervals of` 1.seconds
+            ) {
+                queue = "obi-wan_kenobi"
+                messageTransformer = { toString(Charsets.UTF_8) }
+            } `assert that` {
+                eq("Au secours obi-wan_kenobi, vous êtes notre seul espoir !", it.message)
+            }
+
+            `given number of messages in rabbitmq queue` {
+                queue = "obi-wan_kenobi"
+            } `assert that` {
+                eq(0L, it.total)
+            }
+
         }
-
-        `publish rabbitmq message`("declare that R2D2 might deliver a message to Obi-Wan Kenobi") {
-            publish { "obi-wan_kenobi" } `to exchange` "" `with routing key` "R2D2"
-        }
-
-        `given message from rabbitmq queue`<String>(
-            name = "message from Leia was broadcasted",
-            retry = 10.times `by intervals of` 1.seconds
-        ) {
-            queue = "obi-wan_kenobi"
-            messageTransformer = { toString(Charsets.UTF_8) }
-        } `assert that` {
-            eq("Au secours obi-wan_kenobi, vous êtes notre seul espoir !", it.message)
-        }
-
-    }
 
 }
