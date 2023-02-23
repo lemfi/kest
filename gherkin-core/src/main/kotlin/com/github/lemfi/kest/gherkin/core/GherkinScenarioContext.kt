df684a304acc51b18f3f66f2677724b76f8b04c3
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2023-02-23 23:26:59 +0100

    set context only when step result succeeds

--- gherkin-core/src/main/kotlin/com/github/lemfi/kest/gherkin/core/GherkinScenarioContext.kt
+++ gherkin-core/src/main/kotlin/com/github/lemfi/kest/gherkin/core/GherkinScenarioContext.kt
@@ -1,29 +1,30 @@
 package com.github.lemfi.kest.gherkin.core
 
-import com.github.lemfi.kest.core.model.StandaloneStepPostExecution
+import com.github.lemfi.kest.core.model.IStepPostExecution
 
 interface GherkinContext
 
 private val gherkinContextThreadLocal = ThreadLocal.withInitial { ContextHolder() }
 
 @Suppress("UNCHECKED_CAST")
-infix fun <R, T: GherkinContext> StandaloneStepPostExecution<*, *, R>.`set gherkin scenario context`(l: (T?, R) -> T) = apply {
+infix fun <STEP_POST_EXECUTION: IStepPostExecution<*, RESULT>, RESULT, GHERKIN_CONTEXT : GherkinContext> STEP_POST_EXECUTION.`set gherkin scenario context`(l: (GHERKIN_CONTEXT?, RESULT) -> GHERKIN_CONTEXT): STEP_POST_EXECUTION =
+    apply {
 
-    gherkinContextThreadLocal.get().context.add {
-        l(it as T?, this())
+        gherkinContextThreadLocal.get().context.add { gherkinContext ->
+            runCatching { this() }.getOrNull()?.let { l(gherkinContext as GHERKIN_CONTEXT?, it) } ?: gherkinContext
+        }
     }
-}
 
 @Suppress("UNCHECKED_CAST")
-fun <T: GherkinContext?> `get gherkin scenario context`() = gherkinContextThreadLocal
+fun <GHERKIN_CONTEXT : GherkinContext?> `get gherkin scenario context`() = gherkinContextThreadLocal
     .get()
     .context
-    .fold(null as T) { acc, context ->
-        context(acc) as T
+    .fold(null as GHERKIN_CONTEXT) { acc, context ->
+        context(acc) as GHERKIN_CONTEXT
     }
 
 private class ContextHolder {
-    var context: MutableList<(GherkinContext?)->GherkinContext> = mutableListOf()
+    var context: MutableList<(GherkinContext?) -> GherkinContext?> = mutableListOf()
 }
 
 fun cleanGherkinContext() = gherkinContextThreadLocal.set(ContextHolder())
